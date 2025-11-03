2eec5b040927b96837f6a73ab573fc7edffb258f
Arif Rizky <muhammadarf5555@gmail.com>
2025-11-04 05:29:38 +0700

    Update FoundItemScreen.js

--- Screen/FoundItemScreen.js
+++ Screen/FoundItemScreen.js
@@ -114,7 +114,7 @@ export default function FoundItemScreen({ navigation }) {
 
       {/* HEADER */}
       <LinearGradient
-        colors={['#B38E2F', '#006B3F']}
+        colors={['#dca61dff', '#015c36ff']}
         start={{ x: 0, y: 0 }}
         end={{ x: 1, y: 1 }}
         style={styles.header}
